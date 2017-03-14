c9faaa5bb3129d8c6317e8246c750d6cc53e62c6
Petr Kotas <petr.kotas@gmail.com>
2017-03-14 15:35:42 +0100

    done test tweet atom serializer

--- tests/core/serializers/test_tweet_serializer.py
+++ tests/core/serializers/test_tweet_serializer.py
@@ -1,8 +1,12 @@
+import dateutil.parser
+import feedparser
+
+from twitter_feed.core.tweet import Tweet
 from twitter_feed.core.serializers.tweet_serializer import tweets_to_atom
 
 
 def test_tweets_to_atom_full():
-    tweets_dict = [
+    tweets_list = [
         {
             'created_at': 'Tue Aug 28 21:16:23 +0000 2012',
             'id_str': '240558470661799936',
@@ -61,6 +65,34 @@ def test_tweets_to_atom_full():
         }
     ]
 
-    atom_feed = tweets_to_atom(tweets, 'test.com/feed', 'test.com')
+    tweets = Tweet.tweets_from_list(tweets_list)
 
-    
+    atom_feed = tweets_to_atom(tweets, 'My twitter atom feed', 'test.com/feed', 'test.com')
+    feed = atom_feed.to_string()
+
+    feed_parsed = feedparser.parse(feed)
+
+    assert feed_parsed.feed.title == 'My twitter atom feed'
+
+    for entry in feed_parsed.entries:
+        assert entry.author == tweets_list[0]['user']['screen_name']
+        assert entry.title == tweets_list[0]['user']['name']
+
+        date_dict = dateutil.parser.parse(tweets_list[0]['created_at'])
+        date_feed = dateutil.parser.parse(entry.published)
+        assert date_feed == date_dict
+        date_feed = dateutil.parser.parse(entry.updated)
+        assert date_feed == date_dict
+
+        assert entry.id == 'urn:twitter_id:' + tweets_list[0]['id_str']
+
+        dict_content = tweets_list[0]['text'] + '<br>'
+        dict_content += '<b>Mentions:</b> '
+        for mention in tweets_list[0]['entities']['user_mentions']:
+            dict_content += mention['name'] + '; '
+        dict_content += '<br>'
+        dict_content += '<b>Hashtags:</b> '
+        for hashtag in tweets_list[0]['entities']['hashtags']:
+            dict_content += hashtag['text'] + '; '
+        assert entry.content[0].value == dict_content.strip()
+
