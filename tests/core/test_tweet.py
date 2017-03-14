796c24e418a8258b17071f9714612c0109eb7d42
Petr Kotas <petr.kotas@gmail.com>
2017-03-14 13:09:14 +0100

    added test for from list

--- tests/core/test_tweet.py
+++ tests/core/test_tweet.py
@@ -15,18 +15,20 @@ def test_tweet_full():
             'screen_name': 'oauth_dancer',
             'id_str': '119476949',
         },
-        'mentions': [
-            {
-                'name': 'Cal',
-                'id_str': '17445752',
-                'screen_name': 'Cal'
-            }
-        ],
-        'hashtags': [
-            {
-                'text': 'twitter'
-            }
-        ]
+        'entities': {
+            'mentions': [
+                {
+                    'name': 'Cal',
+                    'id_str': '17445752',
+                    'screen_name': 'Cal'
+                }
+            ],
+            'hashtags': [
+                {
+                    'text': 'twitter'
+                }
+            ]
+        }
     }
 
     test_tweet = Tweet.from_dict(tweet_dict)
@@ -35,12 +37,12 @@ def test_tweet_full():
     assert test_tweet.text == tweet_dict['text']
     assert test_tweet.user == User.from_dict(tweet_dict['user'])
 
-    assert len(test_tweet.mentions) == len(tweet_dict['mentions'])
-    for user_mention, mention in zip(test_tweet.mentions, tweet_dict['mentions']):
+    assert len(test_tweet.mentions) == len(tweet_dict['entities']['mentions'])
+    for user_mention, mention in zip(test_tweet.mentions, tweet_dict['entities']['mentions']):
         assert user_mention == User.from_dict(mention)
 
-    assert len(test_tweet.hashtags) == len(tweet_dict['hashtags'])
-    for tweet_hashtag, hashtag in zip(test_tweet.hashtags, tweet_dict['hashtags']):
+    assert len(test_tweet.hashtags) == len(tweet_dict['entities']['hashtags'])
+    for tweet_hashtag, hashtag in zip(test_tweet.hashtags, tweet_dict['entities']['hashtags']):
         assert tweet_hashtag == Hashtag.from_dict(hashtag)
 
     assert test_tweet.date == datetime.datetime.strptime(tweet_dict['created_at'], '%a %b %d %X %z %Y')
@@ -55,17 +57,19 @@ def test_tweet_incomplete():
             'screen_name': 'oauth_dancer',
             'id_str': '119476949',
         },
-        'mentions': [
-            {
-                'name': 'Cal',
-                'id_str': '17445752'
-            }
-        ],
-        'hashtags': [
-            {
-                'text': 'twitter'
-            }
-        ]
+        'entities': {
+            'mentions': [
+                {
+                    'name': 'Cal',
+                    'id_str': '17445752'
+                }
+            ],
+            'hashtags': [
+                {
+                    'text': 'twitter'
+                }
+            ]
+        }
     }
 
     test_tweet = Tweet.from_dict(tweet_dict)
@@ -74,12 +78,78 @@ def test_tweet_incomplete():
     assert test_tweet.text == tweet_dict['text']
     assert test_tweet.user == User.from_dict(tweet_dict['user'])
 
-    assert len(test_tweet.mentions) == len(tweet_dict['mentions'])
-    for user_mention, mention in zip(test_tweet.mentions, tweet_dict['mentions']):
+    assert len(test_tweet.mentions) == len(tweet_dict['entities']['mentions'])
+    for user_mention, mention in zip(test_tweet.mentions, tweet_dict['entities']['mentions']):
         assert user_mention == User.from_dict(mention)
 
-    assert len(test_tweet.hashtags) == len(tweet_dict['hashtags'])
-    for tweet_hashtag, hashtag in zip(test_tweet.hashtags, tweet_dict['hashtags']):
+    assert len(test_tweet.hashtags) == len(tweet_dict['entities']['hashtags'])
+    for tweet_hashtag, hashtag in zip(test_tweet.hashtags, tweet_dict['entities']['hashtags']):
         assert tweet_hashtag == Hashtag.from_dict(hashtag)
 
     assert test_tweet.date == datetime.datetime.strptime(tweet_dict['created_at'], '%a %b %d %X %z %Y')
+
+
+def test_tweet_list():
+    tweets_list = [
+        {
+            'created_at': 'Tue Aug 28 21:16:23 +0000 2012',
+            'id_str': '240558470661799936',
+            'entities': {
+                'urls': [
+                ],
+                'hashtags': [
+                    {
+                        'text': 'bagr'
+                    }
+                ],
+                'user_mentions': [
+                    {
+                        'name': 'OAuth Dancer',
+                        'screen_name': 'oauth_dancer',
+                        'id_str': '119476949',
+                    }
+                ]
+            },
+            'text': 'just another test',
+            'source': 'OAuth Dancer Reborn',
+            'user': {
+                'name': 'OAuth Dancer',
+                'created_at': 'Wed Mar 03 19:37:35 +0000 2010',
+                'id_str': '119476949',
+                'screen_name': 'oauth_dancer'
+            },
+        },
+        {
+            'created_at': 'Tue Aug 28 21:16:23 +0000 2012',
+            'id_str': '240558470661799936',
+            'entities': {
+                'urls': [
+                ],
+                'hashtags': [
+                    {
+                        'text': 'bagr'
+                    }
+                ],
+                'user_mentions': [
+                    {
+                        'name': 'OAuth Dancer',
+                        'screen_name': 'oauth_dancer',
+                        'id_str': '119476949',
+                    }
+                ]
+            },
+            'text': 'just another test',
+            'source': 'OAuth Dancer Reborn',
+            'user': {
+                'name': 'OAuth Dancer',
+                'created_at': 'Wed Mar 03 19:37:35 +0000 2010',
+                'id_str': '119476949',
+                'screen_name': 'oauth_dancer'
+            },
+        }
+    ]
+
+    tweets = Tweet.tweets_from_list(tweets_list)
+
+    for tweet, tweet_dict in zip(tweets, tweets_list):
+        assert tweet == Tweet.from_dict(tweet_dict)
