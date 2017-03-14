d55d8736169d431d899db8fbab508e3298c31b20
Petr Kotas <petr.kotas@gmail.com>
2017-03-14 18:13:38 +0100

    flask init

---
+++ twitter_feed/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from twitter_feed.config import config
+from twitter_feed.views.rest import rest_view
+
+
+def make_app(conf='dev'):
+    def before_request():
+        '''
+        Stuff to do before any request.
+        Drop everything if not in endpoint.
+        Check login.
+        '''
+        return
+
+    # create the app and init the database with the app
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+
+    app.config.from_object(config.DevConfig)
+    app.before_request(before_request)
+
+    app.register_blueprint(rest_view)
+
+    return app
