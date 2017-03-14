3e767fe0edeab9c5698f2a01a7f0aceba6df2d87
Petr Kotas <petr.kotas@gmail.com>
2017-03-14 18:14:07 +0100

    mock data

---
+++ twitter_feed/mock/tweets.py
@@ -0,0 +1,344 @@
+HOME_TIMELINE = [
+    {
+        'coordinates': None,
+        'truncated': False,
+        'created_at': 'Tue Aug 28 21:16:23 +0000 2012',
+        'favorited': False,
+        'id_str': '240558470661799936',
+        'in_reply_to_user_id_str': None,
+        'entities': {
+            'urls': [
+
+            ],
+            'hashtags': [
+
+            ],
+            'user_mentions': [
+
+            ]
+        },
+        'text': 'just another test',
+        'contributors': None,
+        'id': 240558470661799936,
+        'retweet_count': 0,
+        'in_reply_to_status_id_str': None,
+        'geo': None,
+        'retweeted': False,
+        'in_reply_to_user_id': None,
+        'place': None,
+        'source': 'OAuth Dancer Reborn',
+        'user': {
+            'name': 'OAuth Dancer',
+            'profile_sidebar_fill_color': 'DDEEF6',
+            'profile_background_tile': True,
+            'profile_sidebar_border_color': 'C0DEED',
+            'profile_image_url': 'http://a0.twimg.com/profile_images/730275945/oauth-dancer_normal.jpg',
+            'created_at': 'Wed Mar 03 19:37:35 +0000 2010',
+            'location': 'San Francisco, CA',
+            'follow_request_sent': False,
+            'id_str': '119476949',
+            'is_translator': False,
+            'profile_link_color': '0084B4',
+            'entities': {
+                'url': {
+                    'urls': [
+                        {
+                            'expanded_url': None,
+                            'url': 'http://bit.ly/oauth-dancer',
+                            'indices': [
+                                0,
+                                26
+                            ],
+                            'display_url': None
+                        }
+                    ]
+                },
+                'description': None
+            },
+            'default_profile': False,
+            'url': 'http://bit.ly/oauth-dancer',
+            'contributors_enabled': False,
+            'favourites_count': 7,
+            'utc_offset': None,
+            'profile_image_url_https': 'https://si0.twimg.com/profile_images/730275945/oauth-dancer_normal.jpg',
+            'id': 119476949,
+            'listed_count': 1,
+            'profile_use_background_image': True,
+            'profile_text_color': '333333',
+            'followers_count': 28,
+            'lang': 'en',
+            'protected': False,
+            'geo_enabled': True,
+            'notifications': False,
+            'description': '',
+            'profile_background_color': 'C0DEED',
+            'verified': False,
+            'time_zone': None,
+            'profile_background_image_url_https': 'https://si0.twimg.com/profile_background_images/80151733/oauth-dance.png',
+            'statuses_count': 166,
+            'profile_background_image_url': 'http://a0.twimg.com/profile_background_images/80151733/oauth-dance.png',
+            'default_profile_image': False,
+            'friends_count': 14,
+            'following': False,
+            'show_all_inline_media': False,
+            'screen_name': 'oauth_dancer'
+        },
+        'in_reply_to_screen_name': None,
+        'in_reply_to_status_id': None
+    },
+    {
+        'coordinates': {
+            'coordinates': [
+                -122.25831,
+                37.871609
+            ],
+            'type': 'Point'
+        },
+        'truncated': False,
+        'created_at': 'Tue Aug 28 21:08:15 +0000 2012',
+        'favorited': False,
+        'id_str': '240556426106372096',
+        'in_reply_to_user_id_str': None,
+        'entities': {
+            'urls': [
+                {
+                    'expanded_url': 'http://blogs.ischool.berkeley.edu/i290-abdt-s12/',
+                    'url': 'http://t.co/bfj7zkDJ',
+                    'indices': [
+                        79,
+                        99
+                    ],
+                    'display_url': 'blogs.ischool.berkeley.edu/i290-abdt-s12/'
+                }
+            ],
+            'hashtags': [
+
+            ],
+            'user_mentions': [
+                {
+                    'name': 'Cal',
+                    'id_str': '17445752',
+                    'id': 17445752,
+                    'indices': [
+                        60,
+                        64
+                    ],
+                    'screen_name': 'Cal'
+                },
+                {
+                    'name': 'Othman Laraki',
+                    'id_str': '20495814',
+                    'id': 20495814,
+                    'indices': [
+                        70,
+                        77
+                    ],
+                    'screen_name': 'othman'
+                }
+            ]
+        },
+        'text': 'lecturing at the \'analyzing big data with twitter\' class at @cal with @othman  http://t.co/bfj7zkDJ',
+        'contributors': None,
+        'id': 240556426106372096,
+        'retweet_count': 3,
+        'in_reply_to_status_id_str': None,
+        'geo': {
+            'coordinates': [
+                37.871609,
+                -122.25831
+            ],
+            'type': 'Point'
+        },
+        'retweeted': False,
+        'possibly_sensitive': False,
+        'in_reply_to_user_id': None,
+        'place': {
+            'name': 'Berkeley',
+            'country_code': 'US',
+            'country': 'United States',
+            'attributes': {
+            },
+            'url': 'http://api.twitter.com/1/geo/id/5ef5b7f391e30aff.json',
+            'id': '5ef5b7f391e30aff',
+            'bounding_box': {
+                'coordinates': [
+                    [
+                        [
+                            -122.367781,
+                            37.835727
+                        ],
+                        [
+                            -122.234185,
+                            37.835727
+                        ],
+                        [
+                            -122.234185,
+                            37.905824
+                        ],
+                        [
+                            -122.367781,
+                            37.905824
+                        ]
+                    ]
+                ],
+                'type': 'Polygon'
+            },
+            'full_name': 'Berkeley, CA',
+            'place_type': 'city'
+        },
+        'source': 'Safari on iOS',
+        'user': {
+            'name': 'Raffi Krikorian',
+            'profile_sidebar_fill_color': 'DDEEF6',
+            'profile_background_tile': False,
+            'profile_sidebar_border_color': 'C0DEED',
+            'profile_image_url': 'http://a0.twimg.com/profile_images/1270234259/raffi-headshot-casual_normal.png',
+            'created_at': 'Sun Aug 19 14:24:06 +0000 2007',
+            'location': 'San Francisco, California',
+            'follow_request_sent': False,
+            'id_str': '8285392',
+            'is_translator': False,
+            'profile_link_color': '0084B4',
+            'entities': {
+                'url': {
+                    'urls': [
+                        {
+                            'expanded_url': 'http://about.me/raffi.krikorian',
+                            'url': 'http://t.co/eNmnM6q',
+                            'indices': [
+                                0,
+                                19
+                            ],
+                            'display_url': 'about.me/raffi.krikorian'
+                        }
+                    ]
+                },
+                'description': {
+                    'urls': [
+
+                    ]
+                }
+            },
+            'default_profile': True,
+            'url': 'http://t.co/eNmnM6q',
+            'contributors_enabled': False,
+            'favourites_count': 724,
+            'utc_offset': -28800,
+            'profile_image_url_https': 'https://si0.twimg.com/profile_images/1270234259/raffi-headshot-casual_normal.png',
+            'id': 8285392,
+            'listed_count': 619,
+            'profile_use_background_image': True,
+            'profile_text_color': '333333',
+            'followers_count': 18752,
+            'lang': 'en',
+            'protected': False,
+            'geo_enabled': True,
+            'notifications': False,
+            'description': 'Director of @twittereng\'s Platform Services. I break things.',
+            'profile_background_color': 'C0DEED',
+            'verified': False,
+            'time_zone': 'Pacific Time (US & Canada)',
+            'profile_background_image_url_https': 'https://si0.twimg.com/images/themes/theme1/bg.png',
+            'statuses_count': 5007,
+            'profile_background_image_url': 'http://a0.twimg.com/images/themes/theme1/bg.png',
+            'default_profile_image': False,
+            'friends_count': 701,
+            'following': True,
+            'show_all_inline_media': True,
+            'screen_name': 'raffi'
+        },
+        'in_reply_to_screen_name': None,
+        'in_reply_to_status_id': None
+    },
+    {
+        'coordinates': None,
+        'truncated': False,
+        'created_at': 'Tue Aug 28 19:59:34 +0000 2012',
+        'favorited': False,
+        'id_str': '240539141056638977',
+        'in_reply_to_user_id_str': None,
+        'entities': {
+            'urls': [
+
+            ],
+            'hashtags': [
+
+            ],
+            'user_mentions': [
+
+            ]
+        },
+        'text': 'You\'d be right more often if you thought you were wrong.',
+        'contributors': None,
+        'id': 240539141056638977,
+        'retweet_count': 1,
+        'in_reply_to_status_id_str': None,
+        'geo': None,
+        'retweeted': False,
+        'in_reply_to_user_id': None,
+        'place': None,
+        'source': 'web',
+        'user': {
+            'name': 'Taylor Singletary',
+            'profile_sidebar_fill_color': 'FBFBFB',
+            'profile_background_tile': True,
+            'profile_sidebar_border_color': '000000',
+            'profile_image_url': 'http://a0.twimg.com/profile_images/2546730059/f6a8zq58mg1hn0ha8vie_normal.jpeg',
+            'created_at': 'Wed Mar 07 22:23:19 +0000 2007',
+            'location': 'San Francisco, CA',
+            'follow_request_sent': False,
+            'id_str': '819797',
+            'is_translator': False,
+            'profile_link_color': 'c71818',
+            'entities': {
+                'url': {
+                    'urls': [
+                        {
+                            'expanded_url': 'http://www.rebelmouse.com/episod/',
+                            'url': 'http://t.co/Lxw7upbN',
+                            'indices': [
+                                0,
+                                20
+                            ],
+                            'display_url': 'rebelmouse.com/episod/'
+                        }
+                    ]
+                },
+                'description': {
+                    'urls': [
+
+                    ]
+                }
+            },
+            'default_profile': False,
+            'url': 'http://t.co/Lxw7upbN',
+            'contributors_enabled': False,
+            'favourites_count': 15990,
+            'utc_offset': -28800,
+            'profile_image_url_https': 'https://si0.twimg.com/profile_images/2546730059/f6a8zq58mg1hn0ha8vie_normal.jpeg',
+            'id': 819797,
+            'listed_count': 340,
+            'profile_use_background_image': True,
+            'profile_text_color': 'D20909',
+            'followers_count': 7126,
+            'lang': 'en',
+            'protected': False,
+            'geo_enabled': True,
+            'notifications': False,
+            'description': 'Reality Technician, Twitter API team, synthesizer enthusiast; a most excellent adventure in timelines. I know it\'s hard to believe in something you can\'t see.',
+            'profile_background_color': '000000',
+            'verified': False,
+            'time_zone': 'Pacific Time (US & Canada)',
+            'profile_background_image_url_https': 'https://si0.twimg.com/profile_background_images/643655842/hzfv12wini4q60zzrthg.png',
+            'statuses_count': 18076,
+            'profile_background_image_url': 'http://a0.twimg.com/profile_background_images/643655842/hzfv12wini4q60zzrthg.png',
+            'default_profile_image': False,
+            'friends_count': 5444,
+            'following': True,
+            'show_all_inline_media': True,
+            'screen_name': 'episod'
+        },
+        'in_reply_to_screen_name': None,
+        'in_reply_to_status_id': None
+    }
+]
