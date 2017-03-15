c9015439d3f8ed350ec19f8a83c56cc57bfade4c
Petr Kotas <petr.kotas@gmail.com>
2017-03-15 10:38:49 +0100

    twitter api with oauth

--- twitter_feed/twitter/api.py
+++ twitter_feed/twitter/api.py
@@ -1,6 +1,9 @@
 import typing
+from urllib.parse import urljoin
 
 import requests
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
 
 from twitter_feed.mock.tweets import HOME_TIMELINE, USER_TIMELINE
 
@@ -30,11 +33,30 @@ class MockAPI:
 
 class TwitterAPI:
 
+    def __init__(self, api_url, api_key, api_secret):
+        self.api_url = api_url
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+        client = BackendApplicationClient(client_id=self.api_key)
+        oauth = OAuth2Session(client=client)
+
+        self.auth_token = oauth.fetch_token(token_url=self.api_url, client_id=self.api_key, client_secret=self.api_secret)
+
+    def call_api(self, method=None, params=None):
+        headers = {'Authorization': 'Bearer ' + self.auth_token}
+
+        url = urljoin(self.api_url, method)
+
+        response = requests.get(url, headers=headers, params=params)
+
+        return response
+
     def get_home_timeline(self) -> typing.List[dict]:
         return []
 
     def get_user_timeline(self, user: str = None) -> typing.List[dict]:
-        return []
+        return self.call_api(method='statuses/user_timeline.json', params={'screen_name': user, 'count': 30})
 
     def get_hashtag_timeline(self, hashtag: str = None) -> typing.List[dict]:
         return []
