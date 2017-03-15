c1393ae34f87ee538e2b78470705815c8c67d710
Petr Kotas <petr.kotas@gmail.com>
2017-03-15 09:07:09 +0100

    test for endpoint, for basic funkcionality

--- tests/views/test_rest.py
+++ tests/views/test_rest.py
@@ -6,7 +6,7 @@ import feedparser
 
 from twitter_feed import make_app
 from twitter_feed.core.tweet import Tweet
-from twitter_feed.mock.tweets import HOME_TIMELINE
+from twitter_feed.mock.tweets import HOME_TIMELINE, USER_TIMELINE
 
 
 @pytest.fixture
@@ -47,3 +47,60 @@ def test_home_timeline(test_client):
             dict_content += hashtag.text + '; '
         assert entry.content[0].value == dict_content.strip()
 
+
+def test_user_timeline(test_client):
+    response = test_client.get('/rest/users/test')
+    feed = feedparser.parse(response.data)
+    tweets = Tweet.tweets_from_list(USER_TIMELINE)
+
+    assert feed.feed.title == 'Feed for test'
+
+    for entry, tweet in zip(feed.entries, tweets):
+        assert entry.author == tweet.user.screen_name
+        assert entry.title == tweet.user.name
+
+        date_feed = dateutil.parser.parse(entry.published)
+        assert date_feed == tweet.date
+        date_feed = dateutil.parser.parse(entry.updated)
+        assert date_feed == tweet.date
+
+        assert entry.id == 'urn:twitter_id:' + tweet.id_
+
+        dict_content = tweet.text + '<br>'
+        dict_content += '<b>Mentions:</b> '
+        for mention in tweet.mentions:
+            dict_content += mention.name + '; '
+        dict_content += '<br>'
+        dict_content += '<b>Hashtags:</b> '
+        for hashtag in tweet.hashtags:
+            dict_content += hashtag.text + '; '
+        assert entry.content[0].value == dict_content.strip()
+
+
+def test_hashtag_timeline(test_client):
+    response = test_client.get('/rest/hashtags/test')
+    feed = feedparser.parse(response.data)
+    tweets = Tweet.tweets_from_list(USER_TIMELINE)
+
+    assert feed.feed.title == 'Feed for test'
+
+    for entry, tweet in zip(feed.entries, tweets):
+        assert entry.author == tweet.user.screen_name
+        assert entry.title == tweet.user.name
+
+        date_feed = dateutil.parser.parse(entry.published)
+        assert date_feed == tweet.date
+        date_feed = dateutil.parser.parse(entry.updated)
+        assert date_feed == tweet.date
+
+        assert entry.id == 'urn:twitter_id:' + tweet.id_
+
+        dict_content = tweet.text + '<br>'
+        dict_content += '<b>Mentions:</b> '
+        for mention in tweet.mentions:
+            dict_content += mention.name + '; '
+        dict_content += '<br>'
+        dict_content += '<b>Hashtags:</b> '
+        for hashtag in tweet.hashtags:
+            dict_content += hashtag.text + '; '
+        assert entry.content[0].value == dict_content.strip()
