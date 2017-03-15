605dd089a360f81eeef5fe61388db5c3e755f060
Petr Kotas <petr.kotas@gmail.com>
2017-03-15 17:37:07 +0100

    test user timeline

--- tests/twitter/test_api.py
+++ tests/twitter/test_api.py
@@ -1,7 +1,9 @@
 import pytest
 import requests
 
+from twitter_feed.core.tweet import Tweet
 from twitter_feed.twitter.api import TwitterAPI, TwitterError
+from twitter_feed.mock.tweets import USER_TIMELINE
 
 
 def test_api_auth_fail(monkeypatch):
@@ -53,3 +55,23 @@ def test_api_auth_ok(monkeypatch):
         api = TwitterAPI('url', 'test', 'fail')
 
         assert api.auth_token == 'blabla'
+
+
+def test_api_get_user_timeline_ok(monkeypatch):
+    def fake_get(*args, **kwargs):
+        class Response:
+            status_code = 200
+
+            def json(self):
+                return USER_TIMELINE
+        return Response()
+
+        # replace requests post with my fake post
+        monkeypatch.setattr(requests, 'get', fake_get)
+
+        api = TwitterAPI('url', 'test', 'test')
+        tweets = api.get_user_timeline('test')
+
+        assert len(tweets) == len(USER_TIMELINE)
+        for tweet, tweet_dict in zip(tweets, USER_TIMELINE):
+            assert tweet == Tweet.from_dict(tweet_dict)
