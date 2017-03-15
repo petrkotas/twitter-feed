d4e60924023754b0f482fe0ab08f85a612140644
Petr Kotas <petr.kotas@gmail.com>
2017-03-15 17:25:53 +0100

    test auth fail

---
+++ tests/twitter/test_api.py
@@ -0,0 +1,36 @@
+import pytest
+import requests
+
+from twitter_feed.twitter.api import TwitterAPI, TwitterError
+
+
+def test_api_auth_fail(monkeypatch):
+    def fake_post(*args, **kwargs):
+        class Response:
+            status_code = 403
+
+            def json(self):
+                return {
+                    'errors': [
+                        {
+                            'code': 99,
+                            'message': 'Unable to verify your credentials.'
+                        }
+                    ]
+                }
+        return Response()
+
+    # replace requests post with my fake post
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    with pytest.raises(TwitterError) as excinfo:
+        api = TwitterAPI('url', 'test', 'fail')
+
+    assert excinfo.value.twitter_error == {
+        'errors': [
+            {
+                'code': 99,
+                'message': 'Unable to verify your credentials.'
+            }
+        ]
+    }
